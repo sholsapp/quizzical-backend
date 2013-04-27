8cb3265246e78f3156689854f75bc64fe6b940ec
Stephen Holsapple <sholsapp@gmail.com>
2013-04-27 15:17:02 -0700

    Work from Red Rock cafe.
    Took care of some database design issues, added a custom stylesheet for the
    web frontend and did some work with the API.

--- quizzical/__init__.py
+++ quizzical/__init__.py
@@ -1,10 +1,10 @@
 import logging
 
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, url_for
 from flask.ext.bootstrap import Bootstrap
 
 from quizzical.model import make_conn_str, db
-from quizzical.model import Quiz, Entity, EntityAttr, QuestionFormat, Question
+from quizzical.model import Quiz, Entity, EntityAttr, QuestionFormat, Question, Tag
 
 
 app = Flask(__name__)
@@ -47,46 +47,64 @@ def index():
 # These are logical views on the database tables
 #
 
-
-@app.route('/api/v1/show_quizzes', methods=['GET'])
-def show_quizzes():
-  quizzes = []
-  for quiz in db.session.query(Quiz):
-    quizzes.append({
-      'id': quiz.id,
-      'name': quiz.name,
-      'questions': [q.tojson() for q in quiz.questions],
-    })
-  return jsonify({'rsp': quizzes})
-
+@app.route('/api/v1/make_question/<quizid>', methods=['GET'])
+def make_question(quizid):
+  tags = set()
+  q = db.session.query(Quiz).filter(Quiz.id == quizid).first()
+  for q in q.questions:
+    entity = db.session.query(Entity).filter(Entity.tags.any(tag=q.tag)).all()
+    print q.format.format_string
+    print entity
+    print q.format.format_string % {'attr1': q.attr1, 'attr2': q.attr2}
+    print q.tag
+    print q.attr1
+    print q.attr2
+  return jsonify({'rsp': None})
 
 #
 # These are raw views on the database tables
 #
 @app.route('/api/v1/quizzes', methods=['GET'])
 def quizzes():
-  return jsonify({})
+  quizzes = []
+  for quiz in db.session.query(Quiz):
+    quizzes.append({
+      'id': quiz.id,
+      'Entity.name': quiz.name,
+      'url': 'http://localhost:5000' + url_for('quiz_rest', quizid=quiz.id),
+    })
+  return jsonify({'rsp': quizzes})
 
 
-@app.route('/api/v1/quiz/<quizid>', methods=['GET', 'CREATE', 'PUT', 'DELETE'])
-def quiz_rest():
-  return jsonify({})
+@app.route('/api/v1/quiz/<quizid>', methods=['GET'])
+def quiz_rest(quizid):
+  quiz = db.session.query(Quiz).filter(Quiz.id == quizid).first()
+  return jsonify(quiz.tojson())
 
 
 @app.route('/api/v1/questions', methods=['GET'])
 def questions():
   return jsonify({})
 
-@app.route('/api/v1/question/<questionid>', methods=['GET', 'CREATE', 'PUT', 'DELETE'])
+
+@app.route('/api/v1/question/<questionid>', methods=['GET'])
 def question_rest():
   return jsonify({})
 
 
 @app.route('/api/v1/entities', methods=['GET'])
 def entities():
-  return jsonify({})
+  titties = []
+  for e in db.session.query(Entity):
+    titties.append({
+      'tags': [t.tojson() for t in e.tags],
+      'url': 'http://localhost:5000' + url_for('entity_rest', entityid=e.id),
+    })
+  return jsonify({'rsp': titties})
 
 
-@app.route('/api/v1/entity/<entityid>', methods=['GET', 'CREATE', 'PUT', 'DELETE'])
-def entity_rest():
-  return jsonify({})
+@app.route('/api/v1/entity/<entityid>', methods=['GET'])
+def entity_rest(entityid):
+  e = db.session.query(Entity).filter(Entity.id == entityid).first()
+  return jsonify(e.tojson())
+
--- quizzical/model.py
+++ quizzical/model.py
@@ -2,7 +2,7 @@ from logging import getLogger
 import os
 
 from flask.ext.sqlalchemy import SQLAlchemy
-from sqlalchemy import Column, Integer, String, ForeignKey, LargeBinary
+from sqlalchemy import Column, Integer, String, ForeignKey, Table, Boolean
 from sqlalchemy.orm import relationship
 
 
@@ -12,29 +12,53 @@ log = getLogger(__name__)
 db = SQLAlchemy()
 
 
+tag_association_table = Table('tag_associations', db.metadata,
+  Column('entity_id', Integer, ForeignKey('entity.id')),
+  Column('tag_id', Integer, ForeignKey('tags.id'))
+)
+
+
 class Quiz(db.Model):
   __tablename__ = 'quiz'
   id = Column(Integer, primary_key=True)
   name = Column(String(256))
-  question_cnt = Column(Integer)
   questions = relationship('Question', backref='quiz')
   def __repr__(self):
     return '<Quiz %r %r>' % (self.id, self.name)
-  def __init__(self, name, question_cnt):
+  def __init__(self, name):
     self.name = name
-    self.question_cnt = question_cnt
+  def tojson(self):
+    return {
+      'id': self.id,
+      'name': self.name,
+      'questions': [q.tojson() for q in self.questions],
+    }
+
+
+class Tag(db.Model):
+  __tablename__ = 'tags'
+  id = Column(Integer, primary_key=True)
+  tag = Column(String, nullable=False, unique=True)
+  def __repr__(self):
+    return "<Tag('%s', '%s')>" % (self.id, self.tag)
+  def tojson(self):
+    return self.tag
 
 
 class Entity(db.Model):
   __tablename__ = 'entity'
   id = Column(Integer, primary_key=True)
-  category = Column(String(256))
-  name = Column(String(256))
   attributes = relationship('EntityAttr', backref='entity')
+  tags = relationship('Tag', secondary=tag_association_table)
   def __repr__(self):
-    return '<Entity %r %r>' % (self.id, self.name)
-  def __init__(self, name):
-    self.name = name
+    return '<Entity %r>' % (self.id)
+  def __init__(self, tags):
+    self.tags.extend(tags)
+  def tojson(self):
+    return {
+      'tags': [t.tojson() for t in self.tags],
+      'attributes': [a.tojson() for a in self.attributes],
+    }
 
 
 class EntityAttr(db.Model):
@@ -43,18 +67,25 @@ class EntityAttr(db.Model):
   entity_id = Column(Integer, ForeignKey('entity.id'))
   attr_name = Column(String(256))
   attr_value = Column(String(256))
-  img = Column(LargeBinary)
+  is_img = Column(Boolean(256))
   def __repr__(self):
     return '<EntityAttr %r %r %r %r>' % (self.id, self.entity_id, self.key, self.value)
-  def __init__(self, key, value, img=None):
+  def __init__(self, key, value, img=False):
     self.attr_name = key
     self.attr_value = value
-    self.img = img
+    self.is_img = img
+  def tojson(self):
+    return {
+      'attr_name': self.attr_name,
+      'attr_value': self.attr_value,
+      'img': self.is_img,
+    }
 
 
 class QuestionFormat(db.Model):
   __tablename__ = 'question_format'
   id = Column(Integer, primary_key=True)
+  question_id = Column(Integer, ForeignKey('question.id'))
   format_string = Column(String(256))
   def __repr__(self):
     return '<QuestionFormat %r %r>' % (self.id, self.format_string)
@@ -66,17 +97,21 @@ class Question(db.Model):
   __tablename__ = 'question'
   id = Column(Integer, primary_key=True)
   quiz_id = Column(Integer, ForeignKey('quiz.id'))
-  entity = Column(String(256))
-  attr = Column(String(256))
+  tag = Column(String(256))
+  attr1 = Column(String(256))
+  attr2 = Column(String(256))
   format = relationship('QuestionFormat', backref='question', uselist=False)
   def __repr__(self):
     return '<Question %r %r %r>' % (self.id, self.entity, self.attr)
+  def __init__(self, tag, attr1, attr2):
+    self.tag = tag
+    self.attr1 = attr1
+    self.attr2 = attr2
   def tojson(self):
     return {
-      'id': self.id,
-      'format': self.format.format_string,
-      'entity': self.entity,
-      'attr_key': self.attr,
+      'tag': self.tag,
+      'attr1': self.attr1,
+      'attr2': self.attr2,
     }
 
 
